42826b2b5d6a81213dea2bec64e6090f2d876599
fatmapostaci <fatmapostaci@gmail.com>
2025-04-16 00:49:34 +0300

    Initial commit

---
+++ src/test/java/tests/C03UseDriverClass.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class C03UserDriverClass {
+    //Go to URL: https://opensource-demo.orangehrmlive.com/
+    //Verify title contains OrangeHRM.
+    //Use Driver Class.
+    @Test
+    public void driverClassTest(){
+
+    }
+}
